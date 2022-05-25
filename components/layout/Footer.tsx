732a29247fd571d83b9ad080c527faf977a57cbe
Oliver <46686848+ORJackson@users.noreply.github.com>
2022-05-25 14:22:25 +0100

    2nd commit

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import Link from 'next/link';
+
+export const Footer = () => {
+  return (
+    <footer className="flex flex-col justify-center items-start max-w-screen-lg px-4 mx-auto py-8">
+      <hr className="w-full border-1 border-gray-100 dark:border-gray-800 mb-8" />
+      <div className="w-full grid grid-cols-1 gap-4 pb-16 sm:grid-cols-4">
+        <div className="flex flex-col space-y-4">
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          {/* <Link href="/about">
+            <a>About</a>
+          </Link> */}
+        </div>
+        <div className="flex flex-col space-y-4">
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/ojacksonio"
+          >
+            Twitter
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/ORJackson"
+          >
+            GitHub
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/oliver-jackson-874a7b19b/"
+          >
+            LinkedIn
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/index.css';
+import { Layout } from '../components/layout/Layout';
+import { ThemeProvider } from 'next-themes';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider attribute="class">
+      <Layout>
+          <Component {...pageProps} />
+      </Layout>
+  </ThemeProvider>
+  )
 }
 
-export default MyApp
+export default MyApp;
