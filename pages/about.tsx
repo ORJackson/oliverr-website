e3b88a06f84219964673480a25155a5d62238966
Oliver <46686848+ORJackson@users.noreply.github.com>
2022-05-26 20:50:44 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,20 +9,20 @@ const About = () => {
       </h1>
       <div className="prose md:prose-lg prose-p:text-gray-900 dark:prose-p:text-gray-100 dark:prose-invert prose-gray mb-16">
         <p>
-          Hi, I'm Oliver. I am soon to be an object-oriented software engineer. I'm finishing off a masters in computer science at Swansea University, in Wales, and I'm from the UK.
+          Hi, I&apos;m Oliver. I am soon to be an object-oriented software engineer. I&apos;m finishing off a masters in computer science at Swansea University, in Wales, and I&apos;m from the UK.
          
         </p>
 
         <p>
           I love being outside and am particularly into surfing, skiing, and climbing. 
-          But I also really enjoy the challenges of solving puzzles, which led me to study computer science. 
+          But I also really enjoy the challenges of solving puzzles involved in writing code, which led me to study computer science. 
         </p>
 
         <p>
           My first degree was in Philosophy, after graduating and one too many hospitality jobs I left the UK to travel abroad, I trekked in Nepal, spent some time in India, and started surfing in Indonesia. 
           At that point in my life my number one priority was to fund surf trips, I took a job in steel construction in Western Australia. I was able to work for a few months, and then spend a good few more in Indonesia. 
-          This cycle continued for a while. Before moving back to Europe I took a big trip with some friendsfrom Margaret River up to Darwin, having missed a whole lot of cool sites on the way up due to not having a serious enough car, 
-          I sold my Nissan X-trail and bought a Toyota landcruiser for the way back down. All in all it was around an 8,000 km trip, and I learnt a lot about self-reliance.
+          This cycle continued for a while. Before moving back to Europe I took a big trip with some friends from Margaret River up to Darwin. Having missed a whole lot of cool sites on the way up due to not having a serious enough car, 
+          I sold my Nissan X-trail and bought a Toyota Landcruiser for the way back down. All in all it was around an 8,000 km trip, and I learnt a lot about self-reliance.
           
         </p>
 
@@ -31,14 +31,14 @@ const About = () => {
           I got to know the Netherlands well, as well as quite a few cities in Germany and Belgium.
           When I moved back to the UK I started a residential construction company with a friend. Our ambition was to use modern methods of construction and automate as many business processes as possible. 
           We succeeded in our ambition and ran Spatial Design and Build together for around a year and a half. 
-          However, it proved to be quite a challenging industry within which to compete and we ended up parting on good terms. My partner's abilities as an architect are second to none in the realm of residential design, Spatial is still running and going strong. 
+          However, it proved to be quite a challenging industry within which to compete and we ended up parting on good terms. My partner&apos;s abilities as an architect are second to none in the realm of residential design and Spatial is still going strong. 
           Another very formative experience.
           
         </p>
 
         <p>
-        My experiences in Amsterdam working for a young, fast growing, and technology focussed company, coupled with my business experience and some time spent online learning to code all gave me a taste for the opportunities held in the world of technology, 
-        leading me to enroll in a Master's in Computer Science at Swansea University in Wales. 
+        My experience in Amsterdam working for a young, fast growing, and technology focussed company, coupled with my business experience and some time spent online learning to code all gave me a taste for the opportunities held in the world of technology, 
+        leading me to enroll in a Master&apos;s in Computer Science at Swansea University in Wales. 
 
         </p>
 
