ed67f17eb93612e098e4f5264e004a081ffbb20b
Oliver <46686848+ORJackson@users.noreply.github.com>
2022-07-06 10:26:48 +0100

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,24 @@ import { ThemeProvider } from 'next-themes';
 import 'tailwindcss/tailwind.css';
 import { MdxComponentsProvider } from '../context/mdxContext';
 
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../utils/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+
   return (
   <ThemeProvider attribute="class">
     <MdxComponentsProvider>
@@ -21,3 +37,23 @@ function MyApp({ Component, pageProps }: AppProps) {
 
 
 export default MyApp;
+
+
+
+
+
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return (
+//   <ThemeProvider attribute="class">
+//     <MdxComponentsProvider>
+//         <Layout>
+//             <Component {...pageProps} />
+//         </Layout>
+//     </MdxComponentsProvider>
+//   </ThemeProvider>
+//   )
+// }
+
+
+// export default MyApp;
